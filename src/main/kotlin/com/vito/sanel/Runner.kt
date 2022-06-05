c0a842571606164508655d0d013b3beef7a717ec
Vitaliy Ippolitov <tobestmail@gmail.com>
2022-06-05 13:27:59 +0400

    Skeleton: modules added, simple runner + test. Fix gitignore

---
+++ src/main/kotlin/com/vito/sanel/Runner.kt
@@ -0,0 +1,11 @@
+package com.vito.sanel
+
+class Runner {
+
+    companion object {
+        @JvmStatic fun main(args : Array<String>) {
+            println("Ok")
+        }
+    }
+
+}
---
+++ src/test/kotlin/com/vito/sanel/RunnerTest.kt
@@ -0,0 +1,12 @@
+package com.vito.sanel
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertNotNull
+
+class RunnerTest {
+
+    @Test
+    fun simpleTest() {
+        assertNotNull(this)
+    }
+}
