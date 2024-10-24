38a57fe77e1252d4a524a59f462c43723437eddb
Vitaliy Ippolitov <tobestmail@gmail.com>
2024-10-25 00:00:28 +0300

    Clean method

--- app/src/main/kotlin/com/vito/sanel/BrianLuke.kt
+++ app/src/main/kotlin/com/vito/sanel/BrianLuke.kt
@@ -5,10 +5,6 @@ import kotlin.math.exp
 import kotlin.random.Random
 
 const val DEFAULT_MAX_BOARD_SIZE = 1000
-const val INITIAL_TEMPERATURE = 0.6
-const val FINAL_TEMPERATURE = 0.02
-const val ALPHA = 0.99
-const val STEPS_PER_CHANGE = 400
 
 class BrianLuke {
 
@@ -18,58 +14,6 @@ class BrianLuke {
      * @param boardSize length of solutionXtoY as QueenBoard length
      */
     fun generateBoardAndPrintSolution(boardSize: Int = DEFAULT_MAX_BOARD_SIZE) {
-        var anySolutionFound = false
-        var currentQueenBoardSolution = QueenBoard.randomInit(size = boardSize)
-
-        var newBoardSolution = QueenBoard(solutionXtoY = currentQueenBoardSolution.solutionXtoY.copyOf())
-        var bestQueenBoardSolution = QueenBoard.randomInit(size = boardSize)
-        var bestTemperature = -1.0
-        var currentTemperature = INITIAL_TEMPERATURE
-
-        // this parameter doesn't depend on algorithm but interested in statistics and will be printed to log
-        var acceptedByToleranceCount = 0
-
-        while ((currentTemperature > FINAL_TEMPERATURE) && (bestQueenBoardSolution.energy != 0)) {
-            println("Current temperature is: $currentTemperature")
-
-            repeat(STEPS_PER_CHANGE) {
-                var mustUseNewBoardSolution = false
-                newBoardSolution = newBoardSolution.randomSwapQueens()
-
-                if (newBoardSolution.energy <= currentQueenBoardSolution.energy) {
-                    mustUseNewBoardSolution = true
-                } else {
-                    val randomFloat = Random.nextFloat()
-                    val delta = newBoardSolution.energy - currentQueenBoardSolution.energy
-                    if (exp(-delta / currentTemperature) > randomFloat) {
-                        mustUseNewBoardSolution = true
-                        acceptedByToleranceCount++
-                    }
-                }
-
-                if (mustUseNewBoardSolution) {
-                    currentQueenBoardSolution = QueenBoard(solutionXtoY = newBoardSolution.solutionXtoY.copyOf())
-                    if (currentQueenBoardSolution.energy < bestQueenBoardSolution.energy) {
-                        println("Moving best, new energy: ${currentQueenBoardSolution.energy}, temperature is: $currentTemperature")
-                        bestQueenBoardSolution = QueenBoard(solutionXtoY = currentQueenBoardSolution.solutionXtoY.copyOf())
-                        bestTemperature = currentTemperature
-                        anySolutionFound = true
-                    }
-                } else {
-                    newBoardSolution = QueenBoard(solutionXtoY = currentQueenBoardSolution.solutionXtoY.copyOf())
-                }
-            }
-            currentTemperature *= ALPHA
-        }
-        if (anySolutionFound) {
-            println("Founded solution has bestTemperature: $bestTemperature, acceptedByToleranceCount: $acceptedByToleranceCount")
-            println(bestQueenBoardSolution)
-        } else {
-            println("Warn: no any solution has found for board size: $boardSize")
-        }
-    }
-
-    fun generateBoardAndPrintSolutionN1(boardSize: Int = DEFAULT_MAX_BOARD_SIZE) {
         var current = QueenBoard.randomInit(size = boardSize)
         var best: QueenBoard? = null
 
