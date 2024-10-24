0b99db12e9795b7b8a6a3274fafe002594eafd95
Vitaliy Ippolitov <tobestmail@gmail.com>
2024-10-24 11:07:10 +0300

    Remove multithreading, suspended keyword. Clean utils. Renaming global && make reach QueenBoard for logic instead of utils

--- app/src/main/kotlin/com/vito/sanel/BrianLuke.kt
+++ app/src/main/kotlin/com/vito/sanel/BrianLuke.kt
@@ -1,22 +1,15 @@
 package com.vito.sanel
 
-import com.vito.sanel.models.Board
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.withContext
+import com.vito.sanel.models.QueenBoard
 import kotlin.math.exp
 import kotlin.random.Random
 
-const val DEFAULT_MAX_BOARD_LENGTH = 50
+const val DEFAULT_MAX_BOARD_SIZE = 50
 const val INITIAL_TEMPERATURE = 0.4
 const val FINAL_TEMPERATURE = 0.27
 const val ALPHA = 0.98
 const val STEPS_PER_CHANGE = 400
-const val DEFAULT_ENERGY = 100F
-
-const val PARALLEL_MODE_IS_ON = false
-const val THREAD_COUNT = 5
+const val DEFAULT_ENERGY = 100
 
 /**
  * @method generateBoardAndPrintSolution - calculate and print solution according input, consists common logic of
@@ -32,90 +25,64 @@ const val THREAD_COUNT = 5
 
 class BrianLuke {
 
-    suspend fun generateBoardAndPrintSolution(boardLength: Int = DEFAULT_MAX_BOARD_LENGTH) {
+    fun generateBoardAndPrintSolution(boardSize: Int = DEFAULT_MAX_BOARD_SIZE) {
         var anySolutionFound = false
-        var currentBoardSolution = Board(solution = IntArray(boardLength), energy = DEFAULT_ENERGY).apply {
+        var currentQueenBoardSolution = QueenBoard(solutionXtoY = IntArray(boardSize), energy = DEFAULT_ENERGY).apply {
             initSolution()
             computeAndSetEnergy()
         }
 
-        var newBoardSolution = currentBoardSolution.clone()
-        var bestBoardSolution = Board(solution = intArrayOf(boardLength), energy = DEFAULT_ENERGY)
+        var newBoardSolution = currentQueenBoardSolution.clone()
+        var bestQueenBoardSolution = QueenBoard(solutionXtoY = intArrayOf(boardSize), energy = DEFAULT_ENERGY)
         var bestTemperature = -1.0
         var currentTemperature = INITIAL_TEMPERATURE
 
         // this parameter doesn't depend on algorithm but interested in statistics and will be printed to log
-        var acceptedByToleranceTimes = 0
+        var acceptedByToleranceCount = 0
 
-        while ((currentTemperature > FINAL_TEMPERATURE) && (bestBoardSolution.energy != 0F)) {
+        while ((currentTemperature > FINAL_TEMPERATURE) && (bestQueenBoardSolution.energy != 0)) {
             println("Current temperature is: $currentTemperature")
 
             repeat(STEPS_PER_CHANGE) {
                 var mustUseNewBoardSolution = false
                 newBoardSolution = tweakWork(workSolution = newBoardSolution)
 
-                if (newBoardSolution.energy <= currentBoardSolution.energy) {
+                if (newBoardSolution.energy <= currentQueenBoardSolution.energy) {
                     mustUseNewBoardSolution = true
                 } else {
                     val randomFloat = Random.nextFloat()
-                    val delta = newBoardSolution.energy - currentBoardSolution.energy
+                    val delta = newBoardSolution.energy - currentQueenBoardSolution.energy
                     if (exp(-delta / currentTemperature) > randomFloat) {
                         mustUseNewBoardSolution = true
-                        acceptedByToleranceTimes++
+                        acceptedByToleranceCount++
                     }
                 }
 
                 if (mustUseNewBoardSolution) {
-                    currentBoardSolution = newBoardSolution.clone()
-                    if (currentBoardSolution.energy < bestBoardSolution.energy) {
-                        println("Moving best solution, new energy: ${currentBoardSolution.energy}")
-                        bestBoardSolution = currentBoardSolution.clone()
+                    currentQueenBoardSolution = newBoardSolution.clone()
+                    if (currentQueenBoardSolution.energy < bestQueenBoardSolution.energy) {
+                        println("Moving best solution, new energy: ${currentQueenBoardSolution.energy}")
+                        bestQueenBoardSolution = currentQueenBoardSolution.clone()
                         bestTemperature = currentTemperature
                         anySolutionFound = true
                     }
                 } else {
-                    newBoardSolution = currentBoardSolution.clone()
+                    newBoardSolution = currentQueenBoardSolution.clone()
                 }
             }
             currentTemperature *= ALPHA
         }
         if (anySolutionFound) {
-            bestBoardSolution.printPrettySolution(
-                temperature = bestTemperature,
-                acceptedCount = acceptedByToleranceTimes,
-            )
+            println("Info: bestTemperature: $bestTemperature, acceptedByToleranceCount: $acceptedByToleranceCount")
+            println(bestQueenBoardSolution)
         } else {
-            println("Warn: no any solution has found for board size: $boardLength") // unreachable yet code =)
+            println("Warn: no any solution has found for board size: $boardSize")
         }
     }
 
-    private suspend fun tweakWork(workSolution: Board): Board =
-        if (PARALLEL_MODE_IS_ON) {
-            doTweakWork(List(THREAD_COUNT) { workSolution.clone() })
-        } else {
+    private fun tweakWork(workSolution: QueenBoard): QueenBoard =
             workSolution.apply {
                 tweakSolution()
                 computeAndSetEnergy()
             }
-        }
-
-    private suspend fun doTweakWork(workSolutions: Collection<Board>): Board = withContext(Dispatchers.Default) {
-        val tweakedBoards = workSolutions.map { async { return@async tryTweakAndCompute(it) } }.awaitAll()
-        val minEnergy = tweakedBoards.minOf { it.energy }
-
-        return@withContext tweakedBoards.first { it.energy == minEnergy }
-    }
-
-    private suspend fun tryTweakAndCompute(board: Board): Board =
-        try {
-            withContext(Dispatchers.Default) {
-                board.apply {
-                    tweakSolution()
-                    computeAndSetEnergy()
-                }
-            }
-        } catch (e: Exception) {
-            println("TryTweakAndCompute error handled: ${e.message}")
-            throw IllegalStateException("TryTweakAndCompute error handled: ${e.message}")
-        }
 }
--- app/src/test/kotlin/com/vito/sanel/BrianLukeTest.kt
+++ app/src/test/kotlin/com/vito/sanel/BrianLukeTest.kt
@@ -1,87 +1,74 @@
 package com.vito.sanel
 
-import com.vito.sanel.models.Board
+import com.vito.sanel.models.QueenBoard
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertNotEquals
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class BrianLukeTest {
 
     @Test
     fun `should be different after one tweakSolution call`() {
-        Board(solution = IntArray(DEFAULT_MAX_BOARD_LENGTH)).run {
+        QueenBoard(solutionXtoY = IntArray(DEFAULT_MAX_BOARD_SIZE)).run {
             this.initDiagonalSolution()
-            val initStateSolution = this.solution.copyOf()
+            val initStateSolution = this.solutionXtoY.copyOf()
             this.tweakSolution()
-            assertEquals(initStateSolution.size, this.solution.size)
-            assertFalse(initStateSolution.contentEquals(this.solution))
+            assertEquals(initStateSolution.size, this.solutionXtoY.size)
+            assertFalse(initStateSolution.contentEquals(this.solutionXtoY))
         }
     }
 
     @Test
-    fun `should be different after few tweakSolution call (low possible false test)`() {
-        Board(solution = IntArray(DEFAULT_MAX_BOARD_LENGTH)).run {
+    fun `should be different after few tweakSolution call`() {
+        QueenBoard(solutionXtoY = IntArray(DEFAULT_MAX_BOARD_SIZE)).run {
             this.initDiagonalSolution()
-            val initStateSolution = this.solution.copyOf()
+            val initStateSolution = this.solutionXtoY.copyOf()
             (0..5).forEach { _ -> this.tweakSolution() }
-            assertEquals(initStateSolution.size, this.solution.size)
-            assertFalse(initStateSolution.contentEquals(this.solution))
+            assertEquals(initStateSolution.size, this.solutionXtoY.size)
+            assertFalse(initStateSolution.contentEquals(this.solutionXtoY))
         }
     }
 
     @Test
     fun `computeEnergy should be zero size one because of the edge`() {
-        Board(solution = intArrayOf(0)).run {
+        QueenBoard(solutionXtoY = intArrayOf(0)).run {
             this.computeAndSetEnergy().also {
-                assertEquals(0F, this.energy)
+                assertEquals(0, this.energy)
             }
         }
     }
 
     @Test
     fun `computeEnergy should be zero size two`() {
-        Board(solution = intArrayOf(0, 1)).run {
-            this.computeAndSetEnergy().also { assertEquals(2F, this.energy) }
+        QueenBoard(solutionXtoY = intArrayOf(0, 1)).run {
+            this.computeAndSetEnergy().also { assertEquals(2, this.energy) }
         }
     }
 
     @Test
     fun `computeEnergy shouldn't be zero size three version0`() {
-        Board(solution = intArrayOf(0, 1, 2)).run {
+        QueenBoard(solutionXtoY = intArrayOf(0, 1, 2)).run {
             this.computeAndSetEnergy().also {
-                assertNotEquals(0F, this.energy)
-                assertEquals(6F, this.energy)
+                assertNotEquals(0, this.energy)
+                assertEquals(6, this.energy)
             }
         }
     }
 
     @Test
     fun `computeEnergy shouldn't be zero size three version1`() {
-        Board(solution = intArrayOf(0, 2, 1)).run {
-            this.computeAndSetEnergy().also { assertEquals(2F, this.energy) }
+        QueenBoard(solutionXtoY = intArrayOf(0, 2, 1)).run {
+            this.computeAndSetEnergy().also { assertEquals(2, this.energy) }
         }
     }
 
     @Test
     fun `computeEnergy should be zero`() {
-        Board(solution = intArrayOf(4, 6, 0, 3, 1, 7, 5, 2)).run {
+        QueenBoard(solutionXtoY = intArrayOf(4, 6, 0, 3, 1, 7, 5, 2)).run {
             this.computeAndSetEnergy().also {
-                assertEquals(0F, this.energy)
+                assertEquals(0, this.energy)
             }
-        }
-    }
-
-    @Test
-    fun `should be equals stringView size two`() {
-        Board(solution = intArrayOf(0, 1)).run {
-            this.stringMatrixView().also {
-                assertTrue(it.isNotBlank())
-                assertEquals("[true, false]\n[false, true]\n", it)
-                print(it)
-            }
-            print(this.printPrettySolution(0.0, 0))
         }
     }
 }
